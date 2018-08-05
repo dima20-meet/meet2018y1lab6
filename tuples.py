e1b49cc7652af7ac280df28307a628c5af71a92a
dima20-meet <dima20@meet.mit.edu>
2018-08-05 10:49:24 -0400

    Stopping here

---
+++ tuples.py
@@ -0,0 +1,23 @@
+Python 3.6.5 (default, Apr  1 2018, 05:46:30) 
+[GCC 7.3.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> x,y,z = (3,4,5)
+>>> my_tuple = (3,4,5)
+>>> 
+>>> print (x+y+z)
+12
+>>> print (my_tuple + my_tuple)
+(3, 4, 5, 3, 4, 5)
+>>> print (my_tuple*3)
+(3, 4, 5, 3, 4, 5, 3, 4, 5)
+>>> print (my_tuple[0]==x)
+True
+>>> x=17
+>>> print(x)
+17
+>>> 100000
+100000
+5
+>>> 10000%60
+40
+>>> 
